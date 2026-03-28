d01bf6fdd42a6de40e81e092f1f3cf7092dfd875
Linniel Driver-Williams <LinnielDW@googlemail.com>
2026-03-28 13:41:11 +0100

    fixed missing import

--- ShadowfallCode/Cards/ShadowRegent/Plot.cs
+++ ShadowfallCode/Cards/ShadowRegent/Plot.cs
@@ -3,7 +3,6 @@ using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 using Shadowfall.ShadowfallCode.CardPiles;
 
