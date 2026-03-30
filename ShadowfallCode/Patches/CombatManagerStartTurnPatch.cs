dc7166914d89ad16962c16a404dd38e3123e8664
Linniel Driver-Williams <LinnielDW@googlemail.com>
2026-03-31 00:48:15 +0200

    moved CargoDrawManager to harmony patch

---
+++ ShadowfallCode/Patches/CombatManagerStartTurnPatch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Hooks;
+using MegaCrit.Sts2.Core.Models;
+using Shadowfall.ShadowfallCode.CardPiles;
+
+namespace Shadowfall.ShadowfallCode.Patches;
+
+[HarmonyPatch(typeof(Hook), "AfterPlayerTurnStart", MethodType.Async)]
+public class CombatManagerStartTurnPatch
+{
+    static void Postfix(Player ___player)
+    {
+        var cargoPile = CargoCardPile.CargoPileType.GetPile(___player);
+        if (!cargoPile.IsEmpty)
+        {
+            var card = cargoPile.Cards.FirstOrDefault();
+            if (card != null)
+            {
+                CardPileCmd.Add(card, PileType.Hand);
+            }
+        }
+    }
+}
--- ShadowfallCode/Singletons/CargoDrawManager.cs
+++ ShadowfallCode/Singletons/CargoDrawManager.cs
@@ -1,26 +1,29 @@
-﻿using MegaCrit.Sts2.Core.Commands;
-using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.Entities.Players;
-using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.Models;
-using Shadowfall.ShadowfallCode.CardPiles;
+﻿//TODO: readd when singletonmodels are properly supported (and remove CombatManagerStartTurnPatch).
+// see https://discord.com/channels/309399445785673728/1478917653551906947/1487458524790264070 for more details
 
-namespace Shadowfall.ShadowfallCode.Singletons;
-
-public class CargoDrawManager : SingletonModel
-{
-    public override bool ShouldReceiveCombatHooks => true;
-
-    public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
-    {
-        var cargoPile = CargoCardPile.CargoPileType.GetPile(player);
-        if (!cargoPile.IsEmpty)
-        {
-            var card = cargoPile.Cards.FirstOrDefault();
-            if (card != null)
-            {
-                await CardPileCmd.Add(card, PileType.Hand);
-            }
-        }
-    }
-}
+// using MegaCrit.Sts2.Core.Commands;
+// using MegaCrit.Sts2.Core.Entities.Cards;
+// using MegaCrit.Sts2.Core.Entities.Players;
+// using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+// using MegaCrit.Sts2.Core.Models;
+// using Shadowfall.ShadowfallCode.CardPiles;
+//
+// namespace Shadowfall.ShadowfallCode.Singletons;
+//
+// public class CargoDrawManager : SingletonModel
+// {
+//     public override bool ShouldReceiveCombatHooks => true;
+//
+//     public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
+//     {
+//         var cargoPile = CargoCardPile.CargoPileType.GetPile(player);
+//         if (!cargoPile.IsEmpty)
+//         {
+//             var card = cargoPile.Cards.FirstOrDefault();
+//             if (card != null)
+//             {
+//                 await CardPileCmd.Add(card, PileType.Hand);
+//             }
+//         }
+//     }
+// }
